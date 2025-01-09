3478c077154913f29dddc5ee8768939afde527ce
BhoumikPatidar <bhoumikmnc@gmail.com>
2025-01-09 09:56:39 +0530

    update

--- trial.py
+++ trial.py
@@ -2,17 +2,17 @@
 Module for implementing the Merge Sort algorithm.
 """
 
-def mergesort(lst):
+def mergesort(array):
     """
     Sorts a list in ascending order using the merge sort algorithm.
     
     Args:
-        lst (list): The list to be sorted.
+        array (list): The list to be sorted.
     """
-    if len(lst) > 1:
-        mid = len(lst) // 2
-        left_half = lst[:mid]
-        right_half = lst[mid:]
+    if len(array) > 1:
+        mid = len(array) // 2
+        left_half = array[:mid]
+        right_half = array[mid:]
 
         # Recursively sort both halves
         mergesort(left_half)
@@ -22,25 +22,28 @@ def mergesort(lst):
         i = j = k = 0
         while i < len(left_half) and j < len(right_half):
             if left_half[i] <= right_half[j]:
-                lst[k] = left_half[i]
+                array[k] = left_half[i]
                 i += 1
             else:
-                lst[k] = right_half[j]
+                array[k] = right_half[j]
                 j += 1
             k += 1
 
         while i < len(left_half):
-            lst[k] = left_half[i]
+            array[k] = left_half[i]
             i += 1
             k += 1
 
         while j < len(right_half):
-            lst[k] = right_half[j]
+            array[k] = right_half[j]
             j += 1
             k += 1
 
 if __name__ == "__main__":
-    lst = [5, 5, 2, 4, 1, 6]
-    print("Original List:", lst)
-    mergesort(lst)
-    print("Sorted List:", lst)
+    # Define the list to be sorted
+    input_list = [5, 5, 2, 4, 1, 6]
+    print("Original List:", input_list)
+    
+    # Call the mergesort function
+    mergesort(input_list)
+    print("Sorted List:", input_list)
